a72b9ce6ca92362dc12079208b35defe379a7c6c
Xigui CHEN （陈夕贵） <xigui.chen@citrix.com>
2023-06-09 17:17:01 +0800

    use only one instance

--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -4,7 +4,11 @@ using LoadBalancer;
 
 List<string> WsServerList = new()
 {
-    "https://billion-branch-basics-dg.trycloudflare.com/TcpHub"
+    "https://billion-branch-basics-dg.trycloudflare.com/TcpHub",
+    "https://gm-risk-lf-declaration.trycloudflare.comTcpHub",
+    "https://routines-restaurant-df-driver.trycloudflare.comTcpHub",
+    "https://bench-accessed-canvas-declare.trycloudflare.comTcpHub",
+    "https://bachelor-stages-solely-ad.trycloudflare.comTcpHub"
 };
 Server server = new(WsServerList, 10);
 await server.Run(CancellationToken.None);
